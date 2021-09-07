bd5bbd8bdd3c4db6213c76ac10d1aaed2949afa7
Z <kerenyi.nagy.zoltan@gmail.com>
2021-09-07 11:37:48 +0200

    Create traccar-client.sh

---
+++ shell/traccar-client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+traccar_ip='172.33.33.10'
+traccar_port='5055'
+id='pc001'
+lat='47.1'
+lon='19.1'
+altitude='3'
+
+curl -X POST -H 'content-length: 128' http://$traccar_ip:$traccar_port/?id=$id&lat=$lat&lon=$lon&altitude=$altitude&timestamp=$(date +"%Y-%m-%d %H:%M:%S") || logger '[-] Traccar Client: error while sending GPS data'
+
+exit 0
